5bab30757a5ca78b6ab58c2bf971421c16da818a
Usuariozombie <edupime_98@hotmail.com>
2024-02-15 16:35:36 +0100

    Update AuthController.java

--- src/main/java/com/vedruna/redsocial/sc/security/auth/controllers/AuthController.java
+++ src/main/java/com/vedruna/redsocial/sc/security/auth/controllers/AuthController.java
@@ -15,6 +15,9 @@ import com.vedruna.redsocial.sc.security.auth.model.LoginRequest;
 import com.vedruna.redsocial.sc.security.auth.model.RegisterRequest;
 import com.vedruna.redsocial.sc.security.auth.services.AuthService;
 
+/**
+ * Controlador que maneja las solicitudes relacionadas con la autenticación.
+ */
 @RestController
 @RequestMapping("/auth")
 @CrossOrigin
@@ -22,22 +25,41 @@ public class AuthController {
 
     @Value("${jwt.secret}")
     private String expectedToken;
-    
+
     @Autowired
     private AuthService authService;
 
+    /**
+     * Maneja la solicitud de inicio de sesión.
+     *
+     * @param request Datos de inicio de sesión proporcionados en el cuerpo de la solicitud.
+     * @param token   Token proporcionado en el encabezado de autorización.
+     * @return ResponseEntity que contiene la respuesta de autenticación.
+     */
     @PostMapping(value = "/login")
     public ResponseEntity<AuthResponse> login(@RequestBody LoginRequest request, @RequestHeader("Authorization") String token) {
         validateToken(token);
         return ResponseEntity.ok(authService.login(request));
     }
 
+    /**
+     * Maneja la solicitud de registro de usuario.
+     *
+     * @param request Datos de registro proporcionados en el cuerpo de la solicitud.
+     * @param token   Token proporcionado en el encabezado de autorización.
+     * @return ResponseEntity que contiene la respuesta de autenticación.
+     */
     @PostMapping(value = "/register")
     public ResponseEntity<AuthResponse> register(@RequestBody RegisterRequest request, @RequestHeader("Authorization") String token) {
         validateToken(token);
         return ResponseEntity.ok(authService.register(request));
     }
 
+    /**
+     * Valida si el token proporcionado coincide con el valor esperado.
+     *
+     * @param token Token proporcionado en el encabezado de autorización.
+     */
     private void validateToken(String token) {
         if (!expectedToken.equals(token)) {
             throw new RuntimeException("Token no válido");
