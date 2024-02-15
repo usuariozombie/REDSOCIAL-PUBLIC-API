124f22189d2959c0a2bca0e229e7d2e1e3fddf78
Usuariozombie <edupime_98@hotmail.com>
2024-02-15 16:34:52 +0100

    Update ApplicationConfig.java

--- src/main/java/com/vedruna/redsocial/sc/security/config/ApplicationConfig.java
+++ src/main/java/com/vedruna/redsocial/sc/security/config/ApplicationConfig.java
@@ -14,17 +14,32 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 
 import com.vedruna.redsocial.sc.model.UserRepository;
 
+/**
+ * Configuración de la aplicación relacionada con la autenticación.
+ */
 @Configuration
 public class ApplicationConfig {
 
     @Autowired
     private UserRepository userRepository;
 
+    /**
+     * Configuración del administrador de autenticación.
+     *
+     * @param config Configuración de autenticación proporcionada por Spring.
+     * @return El administrador de autenticación configurado.
+     * @throws Exception Si ocurre un error durante la configuración del administrador de autenticación.
+     */
     @Bean
     public AuthenticationManager authenticationManager(AuthenticationConfiguration config) throws Exception {
         return config.getAuthenticationManager();
     }
 
+    /**
+     * Proveedor de autenticación que utiliza un servicio de detalles de usuario y un codificador de contraseñas.
+     *
+     * @return El proveedor de autenticación configurado.
+     */
     @Bean
     public AuthenticationProvider authenticationProvider() {
         DaoAuthenticationProvider authenticationProvider = new DaoAuthenticationProvider();
@@ -33,12 +48,22 @@ public class ApplicationConfig {
         return authenticationProvider;
     }
 
+    /**
+     * Servicio de detalles de usuario que busca usuarios por nombre de usuario en el repositorio de usuarios.
+     *
+     * @return El servicio de detalles de usuario configurado.
+     */
     @Bean
     public UserDetailsService userDetailService() {
         return username -> userRepository.findByUsername(username)
                 .orElseThrow(() -> new UsernameNotFoundException("User not found"));
     }
 
+    /**
+     * Codificador de contraseñas que utiliza el algoritmo BCrypt.
+     *
+     * @return El codificador de contraseñas configurado.
+     */
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
