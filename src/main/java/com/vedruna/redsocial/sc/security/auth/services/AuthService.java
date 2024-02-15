f5c79f44f3bcc1a00c7c3badee579ff3060f62d0
Usuariozombie <edupime_98@hotmail.com>
2024-02-15 16:37:14 +0100

    Update AuthService.java

--- src/main/java/com/vedruna/redsocial/sc/security/auth/services/AuthService.java
+++ src/main/java/com/vedruna/redsocial/sc/security/auth/services/AuthService.java
@@ -5,6 +5,7 @@ import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+
 import com.vedruna.redsocial.sc.model.Role;
 import com.vedruna.redsocial.sc.model.SecurityUser;
 import com.vedruna.redsocial.sc.model.UserRepository;
@@ -12,6 +13,9 @@ import com.vedruna.redsocial.sc.security.auth.model.LoginRequest;
 import com.vedruna.redsocial.sc.security.auth.model.RegisterRequest;
 import com.vedruna.redsocial.sc.security.auth.model.AuthResponse;
 
+/**
+ * Servicio de autenticación que proporciona métodos para iniciar sesión y registrar usuarios.
+ */
 @Service
 public class AuthService implements AuthServiceI {
 
@@ -27,16 +31,34 @@ public class AuthService implements AuthServiceI {
     @Autowired
     private AuthenticationManager authenticationManager;
 
+    /**
+     * Realiza el proceso de inicio de sesión y genera un token JWT.
+     *
+     * @param request Objeto de solicitud de inicio de sesión que contiene el nombre de usuario y la contraseña.
+     * @return Objeto AuthResponse que contiene el token JWT generado.
+     */
     public AuthResponse login(LoginRequest request) {
         authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(request.getUsername(), request.getPassword()));
-        SecurityUser user=userRepository.findByUsername(request.getUsername()).orElseThrow();
+        SecurityUser user = userRepository.findByUsername(request.getUsername()).orElseThrow();
         return new AuthResponse(jwtService.getToken(user));
     }
 
+    /**
+     * Registra un nuevo usuario y genera un token JWT.
+     *
+     * @param request Objeto de solicitud de registro que contiene la información del nuevo usuario.
+     * @return Objeto AuthResponse que contiene el token JWT generado.
+     */
     public AuthResponse register(RegisterRequest request) {
-    	SecurityUser user = new SecurityUser(request.getUsername(), request.getFirstName(), request.getLastName(), request.getCountry(), passwordEncoder.encode(request.getPassword()), Role.USER);
+        SecurityUser user = new SecurityUser(
+                request.getUsername(),
+                request.getFirstName(),
+                request.getLastName(),
+                request.getCountry(),
+                passwordEncoder.encode(request.getPassword()),
+                Role.USER
+        );
         userRepository.save(user);
         return new AuthResponse(jwtService.getToken(user));
     }
-
 }
